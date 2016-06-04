ff38735b8f6b9dcb223d2292b5fd8f65cae2eae8
CaptainKimothy <mcleodki@onid.oregonstate.edu>
2016-06-03 22:53:28 -0500

    Everything should work now!
    Merged updated index2.php

--- LoL_Site/add_delete.php
+++ LoL_Site/add_delete.php
@@ -9,52 +9,69 @@
 <div id="main">
 
 <p><body>
-  <h3>Add/Delete/Search Database</h3>
+  <!-- important that the input field name is correct, for php to select it's value -->
+  
+  <h3>Add/Update/Search Database</h3>
   <p>Add Champion</p>
-  <form  name="addChamp" method="post" action="index.php?go">
-    <input  type="text" name="name" placeholder="Name">
-    <input  type="text" name="homeworld" placeholder="Homeworld">
-    <input  type="text" name="damage_type" placeholder="Damage Type">
-    <input  type="text" name="role" placeholder="Role">
-    <input  type="text" name="item" placeholder="Item">
-    <input  type="text" name="lane" placeholder="Lane">
-    <input  type="submit" name="addChamp-submit" value="Add">
-  </form>
+  <form  name="addChamp" method="post" action="index2.php?go">
+      <input  type="text" name="name" placeholder="Name">
+      <input  type="text" name="homeworld" placeholder="Homeworld">
+      <input  type="text" name="damage_type" placeholder="Damage Type">
+      <input  type="submit" name="addChamp-submit" value="Add">
+    </form>
 
+    <p>Add Role to Existing Champion</p>	
+    <form  name="addRole" method="post" action="index2.php?go">
+      <input  type="text" name="champion" placeholder="Champion's Name">
+      <input  type="text" name="role" placeholder="Role">
+      <input  type="submit" name="addChampRole-submit" value="Add">
+    </form>
 
-<!-- important that the input field name is correct, for php to select it's value -->
+    <p>Add Item to Existing Champion</p>	
+    <form  name="addRole" method="post" action="index2.php?go">
+      <input  type="text" name="champion" placeholder="Champion's Name">
+      <input  type="text" name="item" placeholder="Item">
+      <input  type="submit" name="addChampItem-submit" value="Add">
+    </form>
 
-  <p>Add Role</p>	
-  <form  name="addRole" method="post" action="">
-    <input  type="text" name="role_name" placeholder="Role">
-    <input  type="submit" name="addRole-submit" value="Add">
-  </form>
+    <p>Add Lane to Existing Champion</p>	
+    <form  name="addRole" method="post" action="index2.php?go">
+      <input  type="text" name="champion" placeholder="Champion's Name">
+      <input  type="text" name="lane" placeholder="Lane">
+      <input  type="submit" name="addChampLane-submit" value="Add">
+    </form>
 
-  <p>Add Item</p>	
-  <form  name="addItem" method="post" action="">
-    <input  type="text" name="item_name" placeholder="Item Name">
-    <input  type="submit" name="addItem-submit" value="Add">
-  </form>
+    <p>Add New Role</p>	
+    <form  name="addRole" method="post" action="index2.php?go">
+      <input  type="text" name="role" placeholder="Role">
+      <input  type="submit" name="addRole-submit" value="Add">
+    </form>
 
-  <p>Add Lane</p>	
-  <form  name="addLane" method="post" action="">
-    <input  type="text" name="lane_name" placeholder="Lane Name">
-    <input  type="submit" name="addLane-submit" value="Add">
-  </form>
+    <p>Add New Item</p>	
+    <form  name="addItem" method="post" action="index2.php?go">
+      <input  type="text" name="name" placeholder="Item Name">
+      <input  type="submit" name="addItem-submit" value="Add">
+    </form>
 
-  <p>Add Homeworld</p>	
-  <form  name="addHomeworld" method="post" action="">
-    <input  type="text" name="homeworld_name" placeholder="Homeworld">
-    <input  type="text" name="homeworld_desc" placeholder="Description">
-    <input  type="submit" name="addHomeworld-submit" value="Add">
-  </form>
+    <p>Add New Lane</p>	
+    <form  name="addLane" method="post" action="index2.php?go">
+      <input  type="text" name="name" placeholder="Lane Name">
+      <input  type="submit" name="addLane-submit" value="Add">
+    </form>
 
+    <p>Add New Homeworld</p>	
+    <form  name="addHomeworld" method="post" action="index.php?go">
+      <input  type="text" name="name" placeholder="Homeworld">
+      <input  type="text" name="description" placeholder="Description">
+      <input  type="submit" name="addHomeworld-submit" value="Add">
+    </form>
 
-  <p>Search for Champion</p>	
-  <form  name="searchDB" method="post" action="">
-    <input  type="text" name="champion_name" placeholder="Name">
-    <input  type="submit" name="searchDB-submit" value="Search">
-  </form>
+	
+    <p>Search for Champion</p>	
+    <form  name="searchDB" method="post" action="index2.php?go">
+      <input  type="text" name="name" placeholder="Name">
+      <input  type="submit" name="searchDB-submit" value="Search">
+    </form>
 
 </div>
 
@@ -67,38 +84,97 @@
     
     if (!empty($_POST['addChamp-submit'])) {
         if (isset($_POST['addChamp-submit'])) {
+        if (isset($_GET['go'])){
                 $homeworld   = $_POST['homeworld'];
                 $name        = $_POST['name'];
                 $damage_type = $_POST['damage_type'];
-                $role        = $_POST['role'];
-                $item        = $_POST['item'];
-                $lane        = $_POST['lane'];
-                
-                
-                //-select  the database to use
-                //$mydb=mysqli_select_db(DB_SERVER);
-                //-query  the database table
-                $query  = "BEGIN TRANSACTION 
-                        DECLARE @newChampID INT;
-                        
-                        DECLARE @homeworld_id int = SELECT homeworld_id FROM Homeworld WHERE homeworld_id = $homeworld;
-                        INSERT INTO champion (homeworld_id, name, damage_type) VALUES (@homeworld_id, '$name', '$damage_type');
-                        SELECT @newChampID = SCOPE_IDENTITY();
-                        DECLARE @champion_id int = SELECT champion_id FROM champion WHERE champion_id = '$name';
-                        DECLARE @role_id int = SELECT role_id FROM Role WHERE role_id = '$role';
-                        INSERT INTO Roles (champion_id, role_id) VALUES (@champion_id, @role_id);
-                        DECLARE @item_id int = SELECT item_id FROM Item WHERE item_id = '$item';
-                        INSERT INTO Items (item_id, champion_id) VALUES (@item_id, @champion_id);
-                        DECLARE @lane_id int = SELECT lane_id FROM Lane WHERE lane_id = '$lane';
-                        INSERT INTO Lanes (lane_id, champion_id) VALUES (@lane_id, @champion_id);
-                    COMMIT";
-                $result = mysqli_query($connection, $query);
-                echo "Champion Added";
-              
-            
-        }
-    }
+
+                //connect  to the database
+		            $conn=mysqli_connect  (DB_SERVER, DB_USER, DB_PASS, DB_NAME);
+		            if ($conn->connect_errno){
+		            echo "connection failed: ".$conn->connect_error."\n";
+		            exit();
+		            }	
+		            //-query  the database table
+		            $query=("INSERT INTO Champion (homeworld_id, name, damage_type) VALUES ((SELECT homeworld_id FROM Homeworld WHERE Homeworld.homeworld_name LIKE '$homeworld'), '$name', '$damage_type')");
+		            $result=mysqli_query($conn, $query);
+		            echo  "Champion Added";
+		            if(isset($conn)) {
+		                mysqli_close($conn);
+		            }
+		    }
+		  }
+	}
     
+	  if (!empty($_POST['addChampRole-submit'])){
+		    if(isset($_POST['addChampRole-submit'])){
+		    if(isset($_GET['go'])){
+		        $champion=$_POST['champion'];
+		        $role=$_POST['role'];
+		        //connect  to the database
+		        $conn=mysqli_connect  (DB_SERVER, DB_USER, DB_PASS, DB_NAME);
+		        if ($conn->connect_errno){
+		        echo "connection failed: ".$conn->connect_error."\n";
+		        exit();
+		        }	
+		        //-query  the database table
+		        $query="INSERT INTO Roles (champion_id, role_id) VALUES ((SELECT champion_id FROM Champion WHERE name LIKE '$champion'), (SELECT role_id FROM Role WHERE role_name LIKE '$role'))";
+		        $result=mysqli_query($conn, $query);
+		        echo  "Role Added";
+		        if(isset($conn)) {
+			          mysqli_close($conn);
+		        }
+		  }
+		 }
+	}
+	
+	
+	
+	  if (!empty($_POST['addChampItem-submit'])){
+		    if(isset($_POST['addChampItem-submit'])){
+		    if(isset($_GET['go'])){
+		      $champion=$_POST['champion'];
+		      $item=$_POST['item'];
+		      //connect  to the database
+	        $conn=mysqli_connect  (DB_SERVER, DB_USER, DB_PASS, DB_NAME);
+		      if ($conn->connect_errno){
+		      echo "connection failed: ".$conn->connect_error."\n";
+		      exit();
+		      }	
+		      //-query  the database table
+		      $query="INSERT INTO Items (item_id, champion_id) VALUES ((SELECT item_id FROM Item WHERE item_name LIKE '$item'), (SELECT champion_id FROM Champion WHERE name LIKE '$champion'))";
+		      $result=mysqli_query($conn, $query);
+		      echo  "Item Added";
+		      if(isset($conn)) {
+			        mysqli_close($conn);
+		      }
+		 }
+		 }
+	}
+	
+	
+	      if (!empty($_POST['addChampLane-submit'])){
+		      if(isset($_POST['addChampLane-submit'])){
+	      	if(isset($_GET['go'])){
+      		$champion=$_POST['champion'];
+      		$lane=$_POST['lane'];
+      		//connect  to the database
+      		$conn=mysqli_connect  (DB_SERVER, DB_USER, DB_PASS, DB_NAME);
+      		if ($conn->connect_errno){
+      		echo "connection failed: ".$conn->connect_error."\n";
+      		exit();
+      		}	
+      		//-query  the database table
+      		$query="INSERT INTO Lanes (lane_id, champion_id) VALUES ((SELECT lane_id FROM Lane WHERE lane_name LIKE '$lane'), (SELECT champion_id FROM Champion WHERE name LIKE '$champion'))";
+      		$result=mysqli_query($conn, $query);
+      		echo  "Role Added";
+      		if(isset($conn)) {
+      			mysqli_close($conn);
+      		}
+	  	}
+	  	}
+	}
+
 
     // Taken from Skoland example
     if (isset($_POST['addRole-submit'])) {
@@ -177,27 +253,12 @@
                 if (preg_match("/^[  a-zA-Z]+/", $_POST['name'])) {
                     $name = $_POST['name'];
                     
-                    
-                    //-select  the database to use
-                    //$mydb=mysqli_select_db(DB_SERVER);
                     //-query  the database table
-                    //echo "Before query\n";
                     $query = "SELECT DISTINCT name, damage_type, role_name, homeworld_name, lane_name, item_name FROM Champion INNER JOIN Roles on Champion.champion_id = Roles.champion_id INNER JOIN Role ON Roles.role_id = Role.role_id INNER JOIN Homeworld ON Champion.homeworld_id = Homeworld.homeworld_id INNER JOIN Lanes ON Champion.champion_id = Lanes.champion_id INNER JOIN Lane ON Lanes.lane_id = Lane.lane_id INNER JOIN Items ON Champion.champion_id = Items.champion_id INNER JOIN Item ON Items.item_id = Item.item_id WHERE Champion.name LIKE '$name'";
                     //-run  the query against the mysql query function
-                    //echo "after query\n";
-                    
-                    
                     $result = mysqli_query($connection, $query, MYSQLI_USE_RESULT);
-                    //if(!$result){
-                    //  echo "Could not find champion!\n";
-                    
-                    //}
-                    //-create  while loop and loop through result set
-                    //echo "starting to loop through result\n";
-                    //echo "$result\n";
-                    
-                    
-                    
+
+                    // create table to store result
                     echo "<table border='1'>
                             <tr>
                             <th>  Champion  </th>
@@ -207,19 +268,10 @@
                             <th>  Lane  </th>
                             <th>  Item  </th>
                             </tr>";
-                    
+                            
+                    // loop through results and display
                     while ($row = mysqli_fetch_assoc($result)) {
-                        
-                        /*
-                        $name=$row['name'];
-                        $damage_type=$row['damage_type'];
-                        $role_name=$row['role_name'];
-                        $homeworld_name=$row['homeworld_name'];
-                        $lane_name=$row['lane_name'];
-                        $item_name=$row['item_name'];   
-                        */
-                        
-                        
+                      
                         echo "<tr>"; //th
                         echo "<td>" . $row['name'] . "</td>"; //td
                         echo "<td>" . $row['damage_type'] . "</td>";
@@ -228,23 +280,10 @@
                         echo "<td>" . $row['lane_name'] . "</td>";
                         echo "<td>" . $row['item_name'] . "</td>";
                         
-                        
-                        
-                        
-                        //$item_name=$row['item_name'];
                     }
-                    //-display the result of the array
-                    
-                    
                     mysqli_free_result($result);
                     echo "</table>";
-                    
-                    
-                    
                 }
-                //else{
-                //echo  "Please enter a search query";
-                //}
             
         }
     }
